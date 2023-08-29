ddea00b0d93216f92e42a0ef09abec8c360be795
LeonBisters <64695012+LeonBisters@users.noreply.github.com>
2023-08-29 12:57:13 +0200

    completed the registration and login frontend

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -36,7 +36,7 @@ router.post("/login", async (req, res) => {
     }
 
     const token = jwt.sign({id: user.id}, "secret");
-    res.json({token, userId: user._id});
+    res.json({token, userID: user._id});
 });
 
 export {router as userRouter};
